59ea0dad9e3b99e95868ed39d9a5aaffdf948a30
carlos avila <carlosavila.ce@gmail.com>
2014-10-20 16:50:52 -0700

    add settings to project

--- accounts.js
+++ accounts.js
@@ -42,7 +42,6 @@ router
     db.find({ username: user.username }, function (err, data) {
       if (!data.length) {
         db.insert(user, function (err, data) {
-          console.log('insert')
           req.session.userId = data.id;
           res.redirect('/');
         })
@@ -52,7 +51,7 @@ router
     })
   })
   .get('/logout', function (req, res) {
-    req.sesions.userId = null;
+    req.session.userId = null;
     res.redirect('/');
   })
   .use(function (req, res, next) {
@@ -63,6 +62,19 @@ router
       });
     }
     next();
+  })
+  .get('/options/displayed_fields', function (req, res) {
+    if (!req.user) {
+      res.json([]);
+    } else {
+      res.json(req.user.options.displayed_fields || []);
+    }
+  })
+  .post('/options/displayed_fields', function (req, res) {
+    req.user.options.displayed_fields = req.body.fields;
+    db.update({ id: req.user.id }, req.user, function (err, data) {
+      res.json(data[0].options.displayed_fields);
+    });
   });
 
 module.exports = router;
--- public/src/app.js
+++ public/src/app.js
@@ -13,11 +13,18 @@ angular.module('ContactsApp', ['ngRoute', 'ngResource', 'ngMessages'])
         controller: 'SingleController',
         templateUrl: 'views/single.html'
       })
+      .when('/settings', {
+        controller: 'SettingsController',
+        templateUrl: 'views/settings.html'
+      })
       .otherwise({
         redirectTo: '/contacts'
       });
     $locationProvider.html5Mode(true); // sets hashbang
+  })
+  .value('options', {})
+  .run(function (options, Fields) {
+    Fields.get().success(function (data) {
+      options.displayed_fields = data;
+    });
   });
-  // .run(function ($rootScope) {
-  //   $rootScope.message = "Hello Angular!";
-  // });
